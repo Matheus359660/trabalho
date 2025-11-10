aab43ed9c4cb2da3cb9e14f187472165a2aee8bd
Matheus359660 <goncalves.santos.matheus1601@escola.pr.gov.br>
2025-11-10 14:13:25 +0000

    quase terminado

---
+++ script.js
@@ -0,0 +1,9 @@
+function Ligar(){
+    document.getElementById("TV").src ="on.png"
+    console.log("On")
+}
+
+function Desligar(){
+    document.getElementById("TV").src ="off.png"
+    console.log("Off")
+}
