094a5a3d941a11e116d8651a0f7628face2c1ae3
Matheus359660 <goncalves.santos.matheus1601@escola.pr.gov.br>
2025-11-11 12:04:20 +0000

    concluido

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-function Ligar(){
-    document.getElementById("TV").src ="on.png"
-    console.log("On")
+const TV = document.getElementById("TV");
+
+TV.addEventListener("mouseover",ligar);
+TV.addEventListener("mouseout",desligar);
+
+function ligar(){
+    TV.src = "on.png";
+    document.body.style.background = "#000000ff"
+}
+function desligar(){
+    TV.src = "off.png";
+    document.body.style.background = "#111111ff"
+   
 }
 
-function Desligar(){
-    document.getElementById("TV").src ="off.png"
-    console.log("off")
-}
